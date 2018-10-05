c4ba228189e7809c8d02f08819b2fe394672558f
zbshadow <zbshadowx@gmail.com>
2018-10-05 07:27:38 -0400

    Added IOstreams before math is done. Added Sudo Code.

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#using <iostream>
+
+using namespace std;
+
+int main{
+	cout << "Input first sides length ";
+	//cout what is the length of the first side
+	//cin first side
+	//cout what is the length of the second side
+	//cin second side
+	//cout what is the length of the third side
+	//cin third side
+
+	//if first side + second side equals thirdside squared
+		//if first side squared + second side squared equals third side squared
+			//cout this is a right triangle
+		//else
+			//cout this is a traingle but not a right triangle
+	//else if second side plus third side equals first side squared
+		//if third side squared plus second side squared equals first side squared
+			//cout this is a right triang
+		//else
+			// this is a triangle but not a right triangle
+	//else 
+		//cout this is not a triangle
+	return 0;
+};
